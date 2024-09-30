58e25c278a47c9365b5714d3875042927ab5f436
jasonart0 <jasonart0@gmail.com>
2024-10-01 02:45:22 +0500

    reset and global

---
+++ resources/views/components/banner.blade.php
@@ -0,0 +1,8 @@
+<!-- resources/views/components/banner.blade.php -->
+<section class="banner">
+    <div class="banner-content">
+        <h1>Welcome to Flexio</h1>
+        <p>Your one-stop solution for SEO marketing and HR services.</p>
+        <a href="/services" class="btn">Explore Services</a>
+    </div>
+</section>
